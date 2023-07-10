7854c2ca44fa308895340e745c304c4c61151591
Raúl Corchero <rcorchero@aidacanarias.com>
2023-07-10 08:17:40 +0100

    first commit

---
+++ StringCalculatorKata.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace StringCalculatorKata.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
